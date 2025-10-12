aff55c9a8d0eaaee177862fbd0edce3c1e867164
leonardo <leonardoaugusto1000a@gmail.com>
2025-10-11 21:14:16 -0300

    Foi implementado o serviço de login e autenticação

--- app/routers/router.py
+++ app/routers/router.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Email
+from flask_wtf.csrf import CSRFProtect
 
 from app import app
 from app.services.products import ProductsServices
@@ -6,9 +10,36 @@ from app.database.engine import get_connection
 
 conn = get_connection()
 products = ProductsServices(conn)
+csrf = CSRFProtect(app)
 
-@app.route('/', methods=['GET'])
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Senha", validators=[DataRequired()])
+    submit = SubmitField("Entrar")
+
+@app.route("/", methods=["GET"])
 def index():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        if email == "admin@teste.com" and password == "123456":
+            flash("Login realizado com sucesso!", "success")
+            return redirect(url_for("get"))
+        else:
+            flash("Email ou senha incorretos.", "danger")
+            return redirect(url_for("login"))
+
+    return render_template("login.html", form=form)
+
+@app.route('/list_products', methods=['GET'])
+def get():
     result = products.list_all_products()
     return render_template("list_products.html", products=result)
 
@@ -27,11 +58,11 @@ def post(id=None):
             products.update_product(id, request)
         else:
             products.register_products(request)
-        return redirect(url_for("index"))
+        return redirect(url_for("get"))
 
     return render_template("register_products.html", product=product_data)
 
 @app.route("/product/delete/<int:id>", methods=["POST"])
 def delete(id):
     products.delete_product(id)
-    return redirect(url_for("index"))
+    return redirect(url_for("get"))
